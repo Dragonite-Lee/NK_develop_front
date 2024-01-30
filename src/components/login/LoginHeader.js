b76eb29691c335c7ec0b4ebbde244be8c5d35d5e
Dragonite-Lee <ross991222@gmail.com>
2024-01-30 21:27:52 +0900

    기본적인 ui완료-> (모바일 부분 햄버거header와 login api연결 남음)

--- src/components/login/LoginHeader.js
+++ src/components/login/LoginHeader.js
@@ -2,19 +2,19 @@ import NKlogo from "../../assets/NKlogo.png"
 
 const LoginHeader = () => {
   return ( 
-    <header className="flex items-center justify-between mb-[33px]">
+    <header className="tablet:flex items-center justify-between tablet:mb-[33px] mobile:mb-[20px] ">
       <div className="flex items-center gap-7">
-        <img src={NKlogo} alt="NK로고" className="w-[70px] h-[70px]" />
+        <img src={NKlogo} alt="NK로고" className="tablet:w-[70px] tablet:h-[70px] mobile:w-[40px] mobile:h-[40px]" />
         <div>
-          <h1 className="font-nanum_700 text-4xl text-black">
+          <h1 className="font-nanum_700 tablet:text-4xl mobile:text-[20px] text-black">
             NK에듀
           </h1>
-          <h2 className="font-nanum_400 text-xl text-black">
+          <h2 className="font-nanum_400 tablet:text-xl mobile:text-[14px] text-black">
             학생 관리 서비스
           </h2>
         </div>
       </div>
-      <h2 className="font-paybooc_700 text-xl text-grayDark">
+      <h2 className="font-paybooc_700 tablet:text-xl mobile:text-[14px] tablet:mt-0 mobile:mt-[18px] text-grayDark">
         “ 정직하라, 성실하라, 생각하라 ”
       </h2>
     </header>
