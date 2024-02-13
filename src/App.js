309cb5a09511a8caf03cd7990a42838badf9285c
Dragonite-Lee <ross991222@gmail.com>
2024-02-13 14:08:47 +0900

    로컬 로그인 로직 완료 / 로그아웃 미구현

--- src/App.js
+++ src/App.js
@@ -2,28 +2,24 @@
 
 // import { testDataAxios } from "./services/test";
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { useEffect } from 'react';
 
 import Main from './container/Main';
 import Login from './container/Login';
 import './App.css';
+import { refreshTokenApi } from './services/loginApi';
 
 function App() {
-
-  //쿼리 사용예제
-  // const {data} = useQuery({
-  //   queryKey: ['test_data'],
-  //   queryFn: () => testDataAxios()
-  //     .then((res) => res.data),
-  // });
-  // console.log(data)
-  
+  useEffect(() => {
+    refreshTokenApi();
+  }, [])
 
   return (
     <div>
       <BrowserRouter>
         <Routes>
-          <Route exact path="/" element={<Main />}></Route>
-          <Route path="/login" element={<Login />}></Route>
+          <Route exact path="/" element={<Login />}></Route>
+          <Route path="/main" element={<Main />}></Route>
         </Routes>
       </BrowserRouter>
     </div>
