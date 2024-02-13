ef7d54d9cf5e399f1dc5f97c4d5a4af82f76efe2
Dragonite-Lee <ross991222@gmail.com>
2024-02-13 14:14:14 +0900

    student branch에 develop branch pull작업

--- src/services/test.js
+++ src/services/test.js
@@ -7,9 +7,5 @@ export const testDataAxios = async () => {
   const res = await axios.get('https://reqres.in/api/users?page=2')
   
   return res.data;
-<<<<<<< HEAD
 }
 
-=======
-}
->>>>>>> 4c3c391 (숙제 목록 디자인 적용 중)
