93cd83db9f72b8eba75688df8291e6b4d07369b3
kkeunii <greenlee55@naver.com>
2024-05-12 03:34:26 +0900

    학부모: 공지사항 완료, 학습태도 분석 진행 중

--- src/components/parent/behaving/Behaving.js
+++ src/components/parent/behaving/Behaving.js
@@ -4,6 +4,8 @@ import { Link } from "react-router-dom";
 
 import SelectStudent from "../SelectStudent"
 import Chart from "./Chart"
+import Button1 from "./Button1"
+import Button2 from "./Button2"
 
 
 const Behaving = () => {
@@ -118,9 +120,20 @@ const Behaving = () => {
 				<div className="text-[14px] font-nanum_700"> {selectedStudent ? `${selectedStudent.label} 학생의 주간 출석률 및 숙제 완료 현황입니다.`:''} </div>
       </div>
       <div className="mt-[20px] w-[1000px] h-full">
-        <div className="mt-[86px] flex gap-[44px] h-[160px] w-[160px]">
+        <div className = "gap-[44px] flex items-center justify-center" >
           {weekdata.map((item, index) => (
-            <Chart complete={item.complete} amount={item.amount} />
+            <div className="font-nanum_700"> 
+              <div className="h-[160px] w-[160px]">
+                <Chart complete={item.complete} amount={item.amount} />
+              </div>
+              <div className="flex items-center justify-center pb-[23px]" >
+                <Button1 backgroundColor="#789AF4" Color="white" content={item.label} size='16px'/>
+              </div>
+              <div className="flex items-center justify-center gap-[7px]" >
+                <Button2 backgroundColor="#F9F9FC" Color="#969699" content={{ status: "완료", complete: item.complete }}/>
+                <Button2 backgroundColor="#F9F9FC" Color="#969699" content={{ status: "미완료", complete: item.amount-item.complete }}/>
+              </div>
+            </div>
           ))}
         </div>
       </div>
