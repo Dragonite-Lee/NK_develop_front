0b44e7abd18ceb90cd3684747a0fce1e4f524b7a
dragonite-lee <ross1222@naver.com>
2024-07-15 23:52:43 +0900

    서버 Url 변경

--- src/services/index.js
+++ src/services/index.js
@@ -2,7 +2,7 @@ import axios from "axios"
 
 export const client = axios.create({
   // baseURL: process.env.REACT_APP_API,
-  baseURL: "http://localhost:8080/",
+  baseURL: "http://ec2-43-201-63-229.ap-northeast-2.compute.amazonaws.com:8080/",
   headers: {
       "Content-Type": "application/json",
   },
