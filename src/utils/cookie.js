5460d83bee85b02fd35a785b75a36719dd50b191
dragonite-lee <ross1222@naver.com>
2024-07-22 10:10:09 +0900

    cookie 콘솔삽입

--- src/utils/cookie.js
+++ src/utils/cookie.js
@@ -3,8 +3,13 @@ import { Cookies } from "react-cookie";
 const cookies = new Cookies();
 
 export const setCookie = (name, value, options) => {
-  return cookies.set(name, value, {...options})
-}
+  try {
+    cookies.set(name, value, { path: '/', ...options });
+    console.log(`Cookie set: ${name}=${value}`);
+  } catch (error) {
+    console.error('Error setting cookie:', error);
+  }
+};
 
 export const getCookie = (name) => {
   return cookies.get(name)
